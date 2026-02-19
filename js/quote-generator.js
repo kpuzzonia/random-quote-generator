710e64c27e21329d89aac65a611b241ecfe29770
Kari Puzzonia <126693423+kpuzzonia@users.noreply.github.com>
2026-02-19 10:33:28 -0500

    Finish script, update of styling

--- js/quote-generator.js
+++ js/quote-generator.js
@@ -1,17 +1,15 @@
 // button click event listener
 let button = document.getElementById("quote-button");
 let quote = document.getElementById("quote");
+let author = document.getElementById("author");
 
 function handleClick() {
     fetch('https://dummyjson.com/quotes/random')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        quote.innerHTML = '"' + data.quote + '"';
+        author.innerHTML = '-' + data.author;
     })
 }
 
 button.addEventListener("click", handleClick);
-// call API with fetch()
-// parse the JSON response
-// grab quote text and author from the data
-// inject into HTML elements
